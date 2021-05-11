31f60402a2c42ba138bf1c6250691d24f483df8f
deshoup <80497708+deshoup@users.noreply.github.com>
2021-05-11 15:02:36 -0500

    Second edit on TestBranch to r script to test multiple commits

--- testRstudioRepo new script.R
+++ testRstudioRepo new script.R
@@ -3,7 +3,7 @@
   set.seed(25) #sets the starting random number so we all get the same values from the random number generator
   control <- rnorm(n=9,mean=78,sd=15) #Create a random normal distibuted data set
   redBull <- rnorm(n=9,mean=72,sd=10)
-  ginko <- rnorm(n=9,mean=79,sd=15)
+  ginko <- rnorm(n=9,mean=79,sd=20)
   studentData <- data.frame(cbind(control, redBull, ginko))
   
   #need to reshape data so there is a column that identifies the group and then a column that has the measured value
